db4006e95263a09a8bcf3bdcfdf5d350376fd28f
Hicham Ait hsaine <hicham.aithsaine@edu.uiz.ac.ma>
2022-10-21 20:25:55 +0100

    testing of Counting duplicate via main characters

--- src/com/company/Counting_duplicate_characters/CountingDC.java
+++ src/com/company/Counting_duplicate_characters/CountingDC.java
@@ -17,9 +17,10 @@ public class CountingDC {
         Map<Character,Integer> result=new HashMap<>();
         for (int i=0;i<str.length();i++){
             char ch=str.charAt(i);
-            result.compute(ch,(u,v ) -> );
+            result.compute(ch,(k,v) -> (v==null)? 1 :++v);
+
         }
-        return ;
+        return result ;
 
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
-public class Main {
+import com.company.Counting_duplicate_characters.CountingDC;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Map;
+
+public class Main {
+    private static final String TEXT="I'm Hicham ait hsaine ";
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("input text \n" +TEXT +"\n");
+        long startTimeV =System.nanoTime();
+        Map<Character,Integer> duplicate = CountingDC.countingDuplicateCharacters(TEXT);
+
+        System.out.println(System.nanoTime()-startTimeV);
+        System.out.println(Arrays.toString(duplicate.entrySet().toArray()));
+
+
     }
 }
