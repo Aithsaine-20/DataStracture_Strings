4214eeb7569e7779206fb1ffdf12bfcd21413d90
Hicham Ait hsaine <hicham.aithsaine@edu.uiz.ac.ma>
2022-10-21 21:10:07 +0100

    add version 2 & 3

--- src/com/company/Counting_duplicate_characters/CountingDC.java
+++ src/com/company/Counting_duplicate_characters/CountingDC.java
@@ -3,6 +3,8 @@ package com.company.Counting_duplicate_characters;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class CountingDC {
     public CountingDC() {
@@ -22,5 +24,31 @@ public class CountingDC {
         }
         return result ;
 
+    }
+    public static Map<String,Integer> countingDuplicateCharactersV2(String str){
+
+        if(str ==null || str.isEmpty()){
+            throw new IllegalArgumentException("string shoud not be empty");
+        }
+        Map<String,Integer> result=new HashMap<>();
+        for (int i=0 ; i <str.length();i++){
+            int cp = str.codePointAt(i);
+            String ch2 =String.valueOf(Character.toChars(cp));
+            if (Character.charCount(cp)==2 ){
+                i++;
+            }
+            result.compute(ch2, (k,v ) -> (v==null)? 1 : ++v );
+
+        }
+        return result;
+    }
+    public static Map<Character,Long> countingDuplicateCharactersV3(String str ){
+
+        if(str ==null || str.isEmpty()){
+            throw new IllegalArgumentException("string shoud not be empty");
+
+        }
+        Map<Character,Long> result = str.chars().mapToObj( c -> (char) c ).collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+        return result;
     }
 }
