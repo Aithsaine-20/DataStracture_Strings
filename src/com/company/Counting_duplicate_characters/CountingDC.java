cd8b5b50ff8059628ec2e31d240f76bc7123c364
Hicham Ait hsaine <hicham.aithsaine@edu.uiz.ac.ma>
2022-10-21 11:27:53 +0100

    first import

---
+++ src/com/company/Counting_duplicate_characters/CountingDC.java
@@ -0,0 +1,25 @@
+package com.company.Counting_duplicate_characters;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountingDC {
+    public CountingDC() {
+        throw  new AssertionError("cannt be instantiated");
+    }
+
+    public static Map<Character,Integer> countingDuplicateCharacters(String str){
+        if (str==null || str.isEmpty()){
+            //throw new IllegalArgumentException("string shoud not be empty");
+            return Collections.emptyMap();
+        }
+        Map<Character,Integer> result=new HashMap<>();
+        for (int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            result.compute(ch,(u,v ) -> );
+        }
+        return ;
+
+    }
+}
---
+++ src/com/company/Unique_characters/Strings1.java
@@ -0,0 +1,9 @@
+package com.company.Unique_characters;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Strings1 {
+
+
+}
